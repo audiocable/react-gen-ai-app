2e7371f515e29107b0f973d73a7721a3aa092ddd
Sid <s018singh@gmail.com>
2023-08-27 19:39:11 -0500

    migrate gen ai react app to public profile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import askAttLogo from './askatt.png';
 
-function App() {
+const App = () => {
+
+  const [ value, setValue ] = useState(null)
+  const [ message, setMessage ] = useState(null)
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8001/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch(error) {
+      console.error(error)
+    }
+  }
+
+  console.log(value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>Conversation 1</li>
+          <li>Conversation 2</li>
+          <li>Conversation 3</li>
+        </ul>
+        <nav>
+          <p>Created by Sid</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1><img src={askAttLogo} alt="Ask AT&T" style={{ width: '384px', height: '158px' }}/></h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>➤</div>
+          </div>
+          <p className="info">Beta 2023. Your feedback will help us improve.</p>
+        </div>
+      </section>
     </div>
   );
 }
